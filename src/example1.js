7caa3207de85b0b9baaed6bb131a8a22155ae17b
edwardlott <edlott@sbcglobal.net>
2018-06-30 17:52:30 -0500

    Lesson2

--- src/example1.js
+++ src/example1.js
@@ -16,3 +16,21 @@ const changeCalcs = function() {
 	calc2 = calc2b;
 	calc1 = 5;
 }
+
+//Globals and scope
+
+const defineGlobals = function() {
+	global1 = 'global1';
+	let local1 = 'local1';
+	showLocal1Function = function() {
+		console.log(local1);
+	}
+}
+
+const showGlobal1 = function() {
+	console.log(global1);
+}
+
+const showLocal1 = function() {
+	console.log(local1);
+}
