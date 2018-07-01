edb9a37fdc330ac4ee4d6795c89a32147375fae7
edwardlott <edlott@sbcglobal.net>
2018-06-30 21:13:20 -0500

    Lesson6b

--- src/index.js
+++ src/index.js
@@ -6,3 +6,6 @@ let local2 = 'index local2';
 const mlib = require('./module1.js');
 mlib.showLocal1();
 mlib.showLocal2();
+
+const mmlib = require('moment');
+console.log(mmlib().format('MMMM Do YYYY, h:mm:ss a'));
