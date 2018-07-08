a1c1ad49b93ac3ad9dfe43898cb28177042129b9
edwardlott <edlott@sbcglobal.net>
2018-07-07 22:36:12 -0500

    React modules.

---
+++ src/App/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const title = 'Application as a module';
+
+export const App = (props) => (
+    <div>{title}</div>
+);
--- src/rindex.js
+++ src/rindex.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {App} from './App';
 
 const title = 'My Minimal React Webpack Babel Setup';
 
 ReactDOM.render(
-  <div>{title}</div>,
+  <App/>,
   document.getElementById('app')
 );
