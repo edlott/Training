9b1b16c35625c43ecff9b2babeabdf5743a78b24
edwardlott <edlott@sbcglobal.net>
2018-07-08 00:04:19 -0500

    Component classes.

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -3,8 +3,37 @@ import { hot } from 'react-hot-loader'
 
 const title = 'Application as a module';
 
-const App = (props) => (
-    <div>{title}</div>
-);
+class App extends React.Component {
+    render() {
+        return (<div>{`${title} count: ${this.state.count}`}</div>);
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {count: 0};
+
+        console.log('in constructor - initialize state');
+    }
+
+    componentDidMount() {
+        console.log('dom created - start AJAX request');
+        setTimeout(() => {
+            this.setState({count: 1});
+        }, 200);
+    }
+
+    componentDidUpdate() {
+        console.log('Just rendered because of a property/state change - maybe start AJAX');
+        if (this.state.count < 20) {
+            setTimeout(() => {
+                this.setState((prevState) => ({count: prevState.count + 1}));
+            }, 200);
+        }
+    }
+
+    componentWillUnmount() {
+        console.log('unregister stuff');
+    }
+}
 
 export default hot(module)(App);
