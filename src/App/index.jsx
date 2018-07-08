fdd2b5d33492a15df4c62c9c96126b6183385ff0
edwardlott <edlott@sbcglobal.net>
2018-07-08 18:08:24 -0500

    Integrate with react.

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,47 +1,30 @@
 import React from 'react';
 import { hot } from 'react-hot-loader'
-import store from '../store';
 import {addOrder} from '../store/actions';
+import {connect} from "react-redux";
 
 const title = 'Application as a module';
 
-class App extends React.Component {
-    render() {
-        return (<div>{`${title} count: ${this.state.count}`}</div>);
-    }
-
-    constructor(props) {
-        super(props);
-        this.state = {count: 0};
-
-        console.log('in constructor - initialize state');
-    }
-
-    componentDidMount() {
-        console.log('dom created - start AJAX request');
+const App = (props) => {
+    const {orders, addOrder} = props;
+    if (orders.size < 20) {
         setTimeout(() => {
-            this.setState({count: 1});
-            store.dispatch(addOrder({id: 1, name: 'Order 1'}));
+            addOrder({id: orders.size + 1, name: `Order ${orders.size + 1}`})
         }, 200);
+    } else {
+        console.log(JSON.stringify(orders.toJSON(), null, 2));
     }
-
-    componentDidUpdate() {
-        console.log('Just rendered because of a property/state change - maybe start AJAX');
-        if (this.state.count < 20) {
-            setTimeout(() => {
-                this.setState((prevState) => {
-                    store.dispatch(addOrder({id: prevState.count + 1, name: `Order ${prevState.count + 1}`}));
-                    return {count: prevState.count + 1}
-                }); 
-            }, 200);
-        } else {
-            console.log(JSON.stringify(store.getState().toJSON(), null, 2));
-        }
-    }
-
-    componentWillUnmount() {
-        console.log('unregister stuff');
-    }
+    return (<div>{`${title} count: ${orders.size}`}</div>);
 }
 
-export default hot(module)(App);
+const mapStateToProps = (state) => {
+    return { orders: state.get('orders') };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        addOrder: (porder) => dispatch(addOrder(porder))
+  };
+};
+
+export default hot(module)(connect(mapStateToProps, mapDispatchToProps)(App));
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
 import {fromJS} from "immutable";
 
 import {ADD_ORDER, REMOVE_ORDER, CHANGE_ORDER} from "./action-type";
@@ -7,7 +7,7 @@ const initialState = fromJS({
     orders: {}
 });
 
-const rootReducer = (state = initialState, action) => {
+const rootReducer = (state, action) => {
     const orders = state.get('orders');
     switch(action.type) {
         case ADD_ORDER:
@@ -19,9 +19,14 @@ const rootReducer = (state = initialState, action) => {
                 return state.set('orders', orders.set(action.order.id, fromJS(action.order)));
             else
                 return state;
+        default:
+            return state;
     }
 }
 
-const store = createStore(rootReducer);
+const enhancers = compose(
+  window.devToolsExtension && process.env.NODE_ENV !== "production" ? window.devToolsExtension() : f => f
+);
+const store = createStore(rootReducer, initialState, enhancers);
 export default store;
 
