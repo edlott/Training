713ddbae429904235db20a24774f1cb5f54daef4
edwardlott <edlott@sbcglobal.net>
2018-07-07 23:16:25 -0500

    Hot reload

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { hot } from 'react-hot-loader'
 
 const title = 'Application as a module';
 
-export default (props) => (
+const App = (props) => (
     <div>{title}</div>
 );
+
+export default hot(module)(App);
